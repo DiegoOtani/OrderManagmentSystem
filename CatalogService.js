ee536e30ea0fc65e240f19397466e36792ceb84c
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-01-22 09:26:47 -0300

    feat: add getProducts function into CatalogService

--- CatalogService.js
+++ CatalogService.js
@@ -1,5 +1,15 @@
 class CatalogService {
+  constructor() {
+    this.products = [
+      { id: 1, name: "Produto A", price: 100, stock: 10 },
+      { id: 2, name: "Produto B", price: 200, stock: 20 },
+    ];
+  }
 
+  getProducts() {
+    console.log("CatalogService: Retornando lista de produtos.");
+    return this.products;
+  }
 }
 
 module.exports = CatalogService;
